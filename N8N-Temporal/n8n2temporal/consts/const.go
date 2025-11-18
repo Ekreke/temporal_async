33278febda93d241129b6bd7dc1d48fb021d2292
彭明峰 <pengmingfeng@acme.red>
2025-11-18 11:37:41 +0800

    1、workflow异步执行优化，完成continue-as-new以及退出条件优化，待验证

--- N8N-Temporal/n8n2temporal/consts/const.go
+++ N8N-Temporal/n8n2temporal/consts/const.go
@@ -1,5 +1,5 @@
 package consts
 
-const DefaultChannelSize = 10000
-const SelectorMaxFutures = 10000
-const ContinueAsNewSelectTicks = 50000
+const DefaultChannelSize = 10000      // 本地通道默认大小
+const SelectorMaxFutures = 10000      // 选择器最大监听futures数量
+const ContinueAsNewSelectTicks = 5000 // continue-as-new历史事件最大阈值数
